9d540a10c1b850471a2d3d12e699d404c1a8f406
Swarnika Suman <68819451+SwarnikaSuman@users.noreply.github.com>
2024-03-17 19:30:51 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,16 @@
+import xlsxwriter
+
+a=0
+b=1
+workbook = xlsxwriter.Workbook("D:\python practice\printno.xlsx")
+worksheet = workbook.add_worksheet()
+worksheet.write(0,0,a)
+worksheet.write(1,0,b)
+for i in range(2,101):
+    c=a+b
+    b=c
+    a=b
+    worksheet.write(i,0,a)
+    worksheet.write(i+1,0,b)
+    worksheet.write(i+2,0,c)
+workbook.close()
